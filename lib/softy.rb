b0be4ece83ec82bc19e0cfa94dcbd04962b39f84
Ryan Oberholzer <ryan@platform45.com>
2013-03-01 11:56:12 +0200

    Ability to soft delete records - untested

--- lib/softy.rb
+++ lib/softy.rb
@@ -1,5 +1,47 @@
 require "softy/version"
 
 module Softy
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do |base|
+    base.alias_method :destroy!, :destroy
+    base.alias_method :delete!, :delete
+    base.default_scope conditions: { deleted_at: nil }
+  end
+
+  def persisted?
+    !new_record?
+  end
+
+  def has_softy?
+    true
+  end
+
+  def destroy
+    _run_destroy_callbacks { delete }
+  end
+
+  def delete
+    update_column(:deleted_at, Time.now) if !destroyed? && persisted?
+    freeze
+  end
+
+  def restore!
+    update_column(:deleted_at, nil)
+  end
+
+  def destroyed?
+    !self.deleted_at.nil?
+  end
+  alias_method :deleted?, :destroyed?
+
+  class ClassMethods
+    def only_deleted
+      unscoped.where("#{self.table_name}.deleted_at is not null")
+    end
+
+    def with_deleted
+      unscoped
+    end
+  end
 end
